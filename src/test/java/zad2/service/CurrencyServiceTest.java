b12aea5045b19b14b87763d9206f3cbb58501999
Michał Barnat <michalbarnat@gmail.com>
2023-08-09 10:42:35 +0200

    done v.2.1

--- src/test/java/zad2/service/CurrencyServiceTest.java
+++ src/test/java/zad2/service/CurrencyServiceTest.java
@@ -8,12 +8,12 @@ public class CurrencyServiceTest {
 
     @Test
     public void shouldReturn49USDfrom200PLN() {
-        assertEquals(49, CurrencyService.exchange("PLN", "USD", 200), 5.0);
+        assertEquals(49, CurrencyService.exchange("PLN", "USD", 200), 5.1);
     }
 
     @Test
     public void shouldReturn465PLNfrom100CHF() {
-        assertEquals(465, CurrencyService.exchange("CHF", "PLN", 100), 5.0);
+        assertEquals(465, CurrencyService.exchange("CHF", "PLN", 100), 5.1);
     }
 
 }
